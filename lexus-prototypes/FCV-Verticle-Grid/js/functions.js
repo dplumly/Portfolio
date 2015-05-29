d597014248e8803229596ce08a326fbcf075bbe8
Donnie Plumly <donnie.plumly@gmail.com>
2015-05-29 09:50:50 -0700

    first

---
+++ lexus-prototypes/FCV-Verticle-Grid/js/functions.js
@@ -0,0 +1,23 @@
+
+
+// Contents of functions.js
+;(function($) {
+  'use strict';
+  var $body = $('html, body'),
+      content = $('#main').smoothState({
+        // Runs when a link has been activated
+        onStart: {
+          duration: 250, // Duration of our animation
+          render: function (url, $container) {
+            // toggleAnimationClass() is a public method
+            // for restarting css animations with a class
+            content.toggleAnimationClass('is-exiting');
+            // Scroll user to the top
+            $body.animate({
+              scrollTop: 0
+            });
+          }
+        }
+      }).data('smoothState');
+      //.data('smoothState') makes public methods available
+})(jQuery);
---
+++ lexus-prototypes/home-page/js/js template.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $("#testbutton").click(function() {
+    $("div").css("background-color","red");	
+  });
+});
