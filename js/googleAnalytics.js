ea7c86021099c8ec555dc3103da15c59357f8704
Donnie Plumly <donnie.plumly@gmail.com>
2020-06-11 09:15:47 -0400

    google analytics

---
+++ js/googleAnalytics.js
@@ -0,0 +1,8 @@
+//Global site tag (gtag.js) - Google Analytics 
+var async src = "https://www.googletagmanager.com/gtag/js?id=UA-38779390-1"
+
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+
+gtag('config', 'UA-38779390-1');
